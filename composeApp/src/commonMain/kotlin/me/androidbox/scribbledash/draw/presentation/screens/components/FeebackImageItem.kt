5eff40f7e36eeaf74d21b0f9f3fdbe94276aceae
Steve <steve@kitkat@gmail.com>
2025-04-20 21:52:56 +0700

    Angled the 2 canvas in the feebackscreen

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/draw/presentation/screens/components/FeebackImageItem.kt
@@ -0,0 +1,50 @@
+package me.androidbox.scribbledash.draw.presentation.screens.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.androidbox.scribbledash.draw.presentation.DrawingAction
+import me.androidbox.scribbledash.draw.presentation.DrawingState
+import me.androidbox.scribbledash.draw.presentation.VectorData
+
+@Composable
+fun FeedbackImageItem(
+    drawingState: DrawingState,
+    onAction: (DrawingAction) -> Unit,
+    title: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.wrapContentWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            lineHeight = 18.sp,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        DrawingCanvas(
+            modifier = Modifier.size(160.dp),
+            paths = drawingState.paths,
+            currentPath = drawingState.currentPath,
+            onAction = onAction,
+            examplePath = drawingState.examplePath,
+            vectorData = VectorData()
+        )
+    }
+}
