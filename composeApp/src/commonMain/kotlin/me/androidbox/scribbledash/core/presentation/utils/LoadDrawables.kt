9c6bb40171aeef58b0340e3c4e0da781a8259c0f
Steve <steve@kitkat@gmail.com>
2025-04-21 12:56:57 +0700

    Uses enum class to load all the drawables

---
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/core/presentation/utils/LoadDrawables.kt
@@ -0,0 +1,105 @@
+package me.androidbox.scribbledash.core.presentation.utils
+
+import org.jetbrains.compose.resources.DrawableResource
+import scribbledash.composeapp.generated.resources.Res
+import scribbledash.composeapp.generated.resources.alien
+import scribbledash.composeapp.generated.resources.bicycle
+import scribbledash.composeapp.generated.resources.boat
+import scribbledash.composeapp.generated.resources.book
+import scribbledash.composeapp.generated.resources.brushes
+import scribbledash.composeapp.generated.resources.butterfly
+import scribbledash.composeapp.generated.resources.camera
+import scribbledash.composeapp.generated.resources.car
+import scribbledash.composeapp.generated.resources.castle
+import scribbledash.composeapp.generated.resources.cat
+import scribbledash.composeapp.generated.resources.chart
+import scribbledash.composeapp.generated.resources.chevron_left
+import scribbledash.composeapp.generated.resources.clock
+import scribbledash.composeapp.generated.resources.close_circle
+import scribbledash.composeapp.generated.resources.compose_multiplatform
+import scribbledash.composeapp.generated.resources.crown
+import scribbledash.composeapp.generated.resources.cup
+import scribbledash.composeapp.generated.resources.dog
+import scribbledash.composeapp.generated.resources.eggtimer
+import scribbledash.composeapp.generated.resources.envelope
+import scribbledash.composeapp.generated.resources.eye
+import scribbledash.composeapp.generated.resources.fish
+import scribbledash.composeapp.generated.resources.flower
+import scribbledash.composeapp.generated.resources.football_field
+import scribbledash.composeapp.generated.resources.forward
+import scribbledash.composeapp.generated.resources.frog
+import scribbledash.composeapp.generated.resources.glasses
+import scribbledash.composeapp.generated.resources.heart
+import scribbledash.composeapp.generated.resources.helicotper
+import scribbledash.composeapp.generated.resources.home
+import scribbledash.composeapp.generated.resources.hotairballoon
+import scribbledash.composeapp.generated.resources.house
+import scribbledash.composeapp.generated.resources.lighting
+import scribbledash.composeapp.generated.resources.moon
+import scribbledash.composeapp.generated.resources.mountains
+import scribbledash.composeapp.generated.resources.one_round_wonder
+import scribbledash.composeapp.generated.resources.paints
+import scribbledash.composeapp.generated.resources.pencil
+import scribbledash.composeapp.generated.resources.reply
+import scribbledash.composeapp.generated.resources.robot
+import scribbledash.composeapp.generated.resources.rocket
+import scribbledash.composeapp.generated.resources.smiley
+import scribbledash.composeapp.generated.resources.snowflake
+import scribbledash.composeapp.generated.resources.sofa
+import scribbledash.composeapp.generated.resources.star
+import scribbledash.composeapp.generated.resources.train
+import scribbledash.composeapp.generated.resources.umbrella
+import scribbledash.composeapp.generated.resources.whale
+enum class ExampleDrawings(
+    val drawableName: String,
+    val drawableResource: DrawableResource
+) {
+    ALIEN("Alien", Res.drawable.alien),
+    BICYCLE("Bicycle", Res.drawable.bicycle),
+    BOAT("Boat", Res.drawable.boat),
+    BOOK("Book", Res.drawable.book),
+    BRUSHES("Brushes", Res.drawable.brushes),
+    BUTTERFLY("Butterfly", Res.drawable.butterfly),
+    CAMERA("Camera", Res.drawable.camera),
+    CAR("Car", Res.drawable.car),
+    CASTLE("Castle", Res.drawable.castle),
+    CAT("Cat", Res.drawable.cat),
+    CHART("Chart", Res.drawable.chart),
+    CHEVRON_LEFT("Chevron Left", Res.drawable.chevron_left),
+    CLOCK("Clock", Res.drawable.clock),
+    CLOSE_CIRCLE("Close Circle", Res.drawable.close_circle),
+    COMPOSE_MULTIPLATFORM("Compose Multiplatform", Res.drawable.compose_multiplatform),
+    CROWN("Crown", Res.drawable.crown),
+    CUP("Cup", Res.drawable.cup),
+    DOG("Dog", Res.drawable.dog),
+    EGTIMER("Egg Timer", Res.drawable.eggtimer),
+    ENVELOPE("Envelope", Res.drawable.envelope),
+    EYE("Eye", Res.drawable.eye),
+    FISH("Fish", Res.drawable.fish),
+    FLOWER("Flower", Res.drawable.flower),
+    FOOTBALL_FIELD("Football Field", Res.drawable.football_field),
+    FORWARD("Forward", Res.drawable.forward),
+    FROG("Frog", Res.drawable.frog),
+    GLASSES("Glasses", Res.drawable.glasses),
+    HEART("Heart", Res.drawable.heart),
+    HELICOPTER("Helicopter", Res.drawable.helicotper),
+    HOME("Home", Res.drawable.home),
+    HOT_AIR_BALLOON("Hot Air Balloon", Res.drawable.hotairballoon),
+    HOUSE("House", Res.drawable.house),
+    LIGHTING("Lighting", Res.drawable.lighting),
+    MOON("Moon", Res.drawable.moon),
+    MOUNTAINS("Mountains", Res.drawable.mountains),
+    ONE_ROUND_WONDER("One Round Wonder", Res.drawable.one_round_wonder),
+    PAINTS("Paints", Res.drawable.paints),
+    PENCIL("Pencil", Res.drawable.pencil),
+    REPLY("Reply", Res.drawable.reply),
+    ROBOT("Robot", Res.drawable.robot),
+    ROCKET("Rocket", Res.drawable.rocket),
+    SMILEY("Smiley", Res.drawable.smiley),
+    SNOWFLAKE("Snowflake", Res.drawable.snowflake),
+    SOFA("Sofa", Res.drawable.sofa),
+    STAR("Star", Res.drawable.star),
+    TRAIN("Train", Res.drawable.train),
+    UMBRELLA("Umbrella", Res.drawable.umbrella),
+    WHALE("Whale", Res.drawable.whale)
+}
