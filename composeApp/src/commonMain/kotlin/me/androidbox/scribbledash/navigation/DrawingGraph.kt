c76ffa16e0e8c78ea442d6cae3451ddeb0989037
Steve <steve@kitkat@gmail.com>
2025-04-28 14:20:42 +0700

    Adds the navigation to the drawing graph

--- composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/navigation/DrawingGraph.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/navigation/DrawingGraph.kt
@@ -6,16 +6,12 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import androidx.navigation.toRoute
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import me.androidbox.scribbledash.core.presentation.utils.getSharedViewModel
 import me.androidbox.scribbledash.core.presentation.utils.observeEvents
 import me.androidbox.scribbledash.draw.presentation.DrawingEvent
 import me.androidbox.scribbledash.draw.presentation.DrawingViewModel
 import me.androidbox.scribbledash.draw.presentation.FeedbackAction
 import me.androidbox.scribbledash.draw.presentation.FeedbackViewModel
-import me.androidbox.scribbledash.draw.presentation.PaintPath
 import me.androidbox.scribbledash.draw.presentation.screens.DifficultyLevelScreen
 import me.androidbox.scribbledash.draw.presentation.screens.DrawingScreen
 import me.androidbox.scribbledash.draw.presentation.screens.FeedbackScreen
@@ -37,10 +33,10 @@ fun NavGraphBuilder.drawingGraph(navController: NavController) {
                 onBottomNavigationClicked = { scribbleDashCategories ->
                     when(scribbleDashCategories) {
                         ScribbleDashCategories.HOME -> {
-
+                            navController.navigate(Route.HomeScreen)
                         }
                         ScribbleDashCategories.CHART -> {
-                            
+                            navController.navigate(Route.StatisticsScreen)
                         }
                     }
                 }
