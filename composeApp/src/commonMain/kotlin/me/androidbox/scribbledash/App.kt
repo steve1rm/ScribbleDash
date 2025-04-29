a0b391767ed472cecf1c64272d5ec2b117df90d8
steve.mason <steve.mason@sainsburys.co.uk>
2025-04-29 16:38:01 +0100

    fixes issue with the navigation bottom bar

--- composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/App.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/App.kt
@@ -1,27 +1,17 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package me.androidbox.scribbledash
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import me.androidbox.scribbledash.core.presentation.components.ScribbleDashLayout
@@ -30,7 +20,6 @@ import me.androidbox.scribbledash.home.model.listOfNavigationItems
 import me.androidbox.scribbledash.home.screens.HomeNavigationBottomBar
 import me.androidbox.scribbledash.navigation.Route
 import me.androidbox.scribbledash.navigation.drawingGraph
-import me.androidbox.scribbledash.statistics.presentation.StatisticsScreen
 import me.androidbox.scribbledash.theming.ScribbleDashTheme
 
 @Composable
@@ -48,22 +37,6 @@ fun App() {
         ScribbleDashLayout(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background),
-            toolBar = {
-                TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.background
-                    ),
-                    title = {
-                        Text(
-                            text = "ScribbleDash",
-                            fontWeight = FontWeight.W400,
-                            style = MaterialTheme.typography.headlineLarge,
-                            fontSize = 26.sp,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                )
-            },
             content = { paddingValues ->
                 NavHost(
                     navController = navController,
@@ -79,24 +52,29 @@ fun App() {
                         modifier = Modifier.fillMaxWidth(),
                         containerColor = MaterialTheme.colorScheme.surfaceContainerHigh
                     ) {
-                        var selectedIndex by rememberSaveable { mutableStateOf(0) }
+                        var selectedIndex by rememberSaveable { mutableStateOf(1) }
 
                         HomeNavigationBottomBar(
                             listOfNavigationItems = listOfNavigationItems,
                             selectedItemIndex = selectedIndex,
                             onItemClicked = { category ->
                                 selectedIndex = category.ordinal
+
                                 when (category) {
-                                    ScribbleDashCategories.HOME -> {
-                                        navController.navigate(Route.HomeScreen) {
-                                            launchSingleTop = true
-                                            popUpTo(Route.HomeScreen) { inclusive = false }
+                                    ScribbleDashCategories.CHART -> {
+                                        if(currentDestinationRoute?.endsWith(Route.HomeScreen::class.simpleName.toString()) != true) {
+                                            navController.navigate(Route.HomeScreen) {
+                                                launchSingleTop = true
+                                                popUpTo(Route.HomeScreen) { inclusive = false }
+                                            }
                                         }
                                     }
-                                    ScribbleDashCategories.CHART -> {
-                                        navController.navigate(Route.StatisticsScreen) {
-                                            launchSingleTop = true
-                                            popUpTo(Route.HomeScreen)
+                                    ScribbleDashCategories.HOME -> {
+                                        if(currentDestinationRoute?.endsWith(Route.StatisticsScreen::class.simpleName.toString()) != true) {
+                                            navController.navigate(Route.StatisticsScreen) {
+                                                launchSingleTop = true
+                                                popUpTo(Route.HomeScreen)
+                                            }
                                         }
                                     }
                                 }
--- composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/statistics/presentation/StatisticsScreen.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/statistics/presentation/StatisticsScreen.kt
@@ -1,17 +1,24 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package me.androidbox.scribbledash.statistics.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.androidbox.scribbledash.core.presentation.components.ScribbleDashLayout
 import me.androidbox.scribbledash.statistics.presentation.components.StatisticsItem
 import me.androidbox.scribbledash.theming.labelXLarge
@@ -26,10 +33,19 @@ fun StatisticsScreen(
     ScribbleDashLayout(
         modifier = modifier,
         toolBar = {
-            Text(
-                text = "Statistics",
-                style = MaterialTheme.typography.labelXLarge,
-                color = MaterialTheme.colorScheme.onBackground
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                title = {
+                    Text(
+                        text = "Statistics",
+                        fontWeight = FontWeight.W400,
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontSize = 26.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
             )
         },
         content = { paddingValues ->
