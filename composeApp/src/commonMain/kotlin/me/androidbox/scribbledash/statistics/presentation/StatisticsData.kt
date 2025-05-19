5c5b8db9bc94111fdcb238617ffde452d50fe7ed
steve1.rm <steve1.rm@gmail.com>
2025-05-19 22:49:28 +0100

    feat: Add accuracy tracking to StatisticsData
    Adds `speedDrawAccuracy` and `endlessDrawAccuracy` properties to `StatisticsData` to track the accuracy of drawings in Speed Draw and Endless Draw modes respectively.

--- composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/statistics/presentation/StatisticsData.kt
+++ composeApp/src/commonMain/kotlin/me/androidbox/scribbledash/statistics/presentation/StatisticsData.kt
@@ -1,6 +1,8 @@
 package me.androidbox.scribbledash.statistics.presentation
 
 object StatisticsData {
-    var endlessDrawingCount = 0
     var speedDrawCount = 0
+    var speedDrawAccuracy = 0
+    var endlessDrawingCount = 0
+    var endlessDrawAccuracy = 0
 }
